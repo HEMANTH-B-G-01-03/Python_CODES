1e8fd84638b2fb8526c92781f25d354475d3591f
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-04-26 09:37:11 +0530

    strings intro

---
+++ str.py
@@ -0,0 +1,33 @@
+#some of the string 
+
+name = "HEMANTH"
+# string slicing
+print(name[0:5])
+
+
+# practuicle use of if else 
+import  time  
+time_t = time.strftime('%H:%M:%S')
+print(time_t)
+
+
+name1 = "samsung"
+for c in name1:
+    print(c)
+
+
+
+for i in range(10):
+    print("i am number ", i)
+
+j = 15
+while(j>0):
+    print(" i am the value decreasing", j)
+    j-=1
+
+
+
+arr = [1,2,3,4,5,6]
+print(arr[2])
+
+# pyhton supports both the postive and negetive indexs 
