90e31300f7e93ab6040a43ce07d4616a99697812
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-04-26 21:47:01 +0530

    files operaion

---
+++ files_os.py
@@ -0,0 +1,23 @@
+#files operations in the pyhton 
+# bellow is reading the file 
+
+# f = open('demo.txt', 'r')
+
+# text = f.read()
+# print("The text present is ", text)
+
+# f.close()
+
+
+# bellow is wrtitng to the smae file 
+
+# f = open('demo.txt','w')
+
+# f.write('Hello, world!')    
+# f.close()
+
+# the code to append
+# f = open('demo.txt','a')
+
+# f.write('Hello, world!')    
+# f.close()
